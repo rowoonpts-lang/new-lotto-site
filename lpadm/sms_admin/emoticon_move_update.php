2a7a2e8e9bc37794a6a3c6451bfe108f1adf67db
rowoonpts-lang <rowoon.pts@gmail.com>
2026-08-04 05:23:02 +0000

    fix: validate SMS emoticon move actions

--- lpadm/sms_admin/emoticon_move_update.php
+++ lpadm/sms_admin/emoticon_move_update.php
@@ -1,7 +1,16 @@
 <?php
+$sub_menu = "900600";
 include_once('./_common.php');
 
 auth_check($auth[$sub_menu], "r");
+
+$sw = isset($_POST['sw']) ? $_POST['sw'] : '';
+$page = isset($_POST['page']) ? max(0, (int) $_POST['page']) : 0;
+$url = isset($_POST['url']) ? clean_xss_tags($_POST['url'], 1, 1) : '';
+
+if (!in_array($sw, array('move', 'copy'), true)) {
+    alert('잘못된 작업 요청입니다.');
+}
 
 $post_chk_fg_no = isset($_POST['chk_fg_no']) && is_array($_POST['chk_fg_no'])
     ? array_values(array_filter(array_map('intval', $_POST['chk_fg_no'])))
@@ -28,9 +37,13 @@ for ($kk=0;$row = sql_fetch_array($result);$kk++)
         $fg_no = (int) $post_chk_fg_no[$i];
         if( !$fg_no ) continue;
         $group = sql_fetch("select * from {$g5['sms5_form_group_table']} where fg_no = '$fg_no'");
+        if (!$group) continue;
+
+        $fg_member = isset($group['fg_member']) ? (int) $group['fg_member'] : 0;
+
         $sql = " insert into {$g5['sms5_form_table']}
                     set fg_no='$fg_no',
-                        fg_member='".$group['fg_member']."',
+                        fg_member='$fg_member',
                         fo_name='".addslashes($row['fo_name'])."',
                         fo_content='".addslashes($row['fo_content'])."',
                         fo_datetime='".G5_TIME_YMDHIS."' ";
