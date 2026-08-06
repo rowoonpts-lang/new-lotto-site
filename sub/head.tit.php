29f32e90f0c6013a56602eb58b823bc7651c2ce3
rowoonpts-lang <rowoon.pts@gmail.com>
2026-08-06 02:37:21 +0000

    fix: restore legacy lotto theme

--- sub/head.tit.php
+++ sub/head.tit.php
@@ -1,90 +1,145 @@
 <?php
+$sub_top_bg = isset($sub_top_bg) ? (string) $sub_top_bg : '';
+$s01_li2 = isset($s01_li2) ? (string) $s01_li2 : '';
+$sub_top_li1 = isset($sub_top_li1) ? (string) $sub_top_li1 : '';
+$sub_top_li2 = isset($sub_top_li2) ? (string) $sub_top_li2 : '';
+$sub_top_li3 = isset($sub_top_li3) ? (string) $sub_top_li3 : '';
+$sub_tit = isset($sub_tit) ? (string) $sub_tit : '';
+$inner_x = isset($inner_x) ? (string) $inner_x : '';
+$bo_table = isset($bo_table) ? (string) $bo_table : '';
+$co_id = isset($co_id) ? (string) $co_id : '';
+
 	switch($basename){
-		case "about.php":
-			$inner = "inner100";
-			$pad0 = "padding0";
+
+		/* sub0101 */
+		case "sub0101.php":
+			$inner_x = "inner_x";
 		break;
 
-		case "system.php":
-			$sub_top = "system_bg.jpg";
-			$sub_title = "분석시스템";
-			$inner = "inner100";
-			$pad0 = "padding0";
-		break;
-		
-		case "membership.php":
-			$sub_top = "membership_bg.jpg";
-			$sub_title = "멤버쉽";
+		case "sub0102.php":
+			$inner_x = "inner_x";
 		break;
 
-		case "data01.php":
-			$sub_top = "data_bg.jpg";
-			$sub_title = "로또자료실";
+		case "sub0201.php":
+			$inner_x = "inner_x";
+		break;
+
+		case "qalist.php": case "qawrite.php": case "qaview.php":
+			$s01_li2 = "고객센터";
+			$sub_top_li1 = "<li><a href='/bbs/board.php?bo_table=notice'>공지사항</a></li>";
+			$sub_top_li2 = "<li><a href='/bbs/board.php?bo_table=faq'>자주묻는 질문</a></li>";
+			$sub_top_li3 = "<li class='active'><a href='/bbs/qalist.php'>1:1 상담</a></li>";
+			$sub_tit = "1:1상담";
+			$sub_top_bg = "sub_top_bg1";
+		break;
+
+		case "login.php":
+			$s01_li2 = "로그인";
+			$sub_top_li1 = "<li class='active'><a href='/bbs/login.php'>로그인</a></li>";
+			$sub_top_li2 = "<li><a href='/bbs/register.php'>회원가입</a></li>";
+			$sub_tit = "로그인";
+		break;
+
+		case "register.php":
+			$s01_li2 = "회원가입";
+			$sub_top_li1 = "<li><a href='/bbs/login.php'>로그인</a></li>";
+			$sub_top_li2 = "<li class='active'><a href='/bbs/register.php'>회원가입</a></li>";
+			$sub_tit = "회원가입";
+		break;
+
+		case "stats.php":
+			$s01_li2 = "통계분석실";
+			$sub_top_li1 = "<li class='active'><a href='/sub/stats.php'>로또 분석용어</a></li>";
+			$sub_top_li2 = "<li><a href='/sub/stats2.php'>확률과 조합 분석</a></li>";
+			$sub_top_li3 = "<li><a href='/sub/stats3.php'>로또 구입 잘하는법</a></li>";
 			$sub_tit = "로또 분석용어";
-		break;
-		case "data02.php":
-			$sub_top = "data_bg.jpg";
-			$sub_title = "로또자료실";
-			$sub_tit = "확률과 조합 분석";
-		break;
-		case "data03.php":
-			$sub_top = "data_bg.jpg";
-			$sub_title = "로또자료실";
-			$sub_tit = "로또 구입 잘하는 방법";
+			$sub_top_bg = "sub_top_bg4";
 		break;
 
-		case "qalist.php":
-			$sub_top = "cs_bg.jpg";
-			$sub_title = "고객센터";
-			$sub_tit = "1:1 문의";
+		case "stats2.php":
+			$s01_li2 = "통계분석실";
+			$sub_top_li1 = "<li><a href='/sub/stats.php'>로또 분석용어</a></li>";
+			$sub_top_li2 = "<li class='active'><a href='/sub/stats2.php'>확률과 조합 분석</a></li>";
+			$sub_top_li3 = "<li><a href='/sub/stats3.php'>로또 구입 잘하는법</a></li>";
+			$sub_tit = "확률과 조합 분석";
+			$sub_top_bg = "sub_top_bg4";
+		break;
+
+		case "stats3.php":
+			$s01_li2 = "통계분석실";
+			$sub_top_li1 = "<li><a href='/sub/stats.php'>로또 분석용어</a></li>";
+			$sub_top_li2 = "<li><a href='/sub/stats2.php'>확률과 조합 분석</a></li>";
+			$sub_top_li3 = "<li class='active'><a href='/sub/stats3.php'>로또 구입 잘하는법</a></li>";
+			$sub_tit = "로또 구입 잘하는법";
+			$sub_top_bg = "sub_top_bg4";
+		break;
+
+		case "my_lotto.php":
+			$s01_li2 = "마이페이지";
+			$sub_top_li1 = "<li class='sub_top_li sub_top_li1 active'><a onClick='tabBtn(\"1\")'>나의 당첨현황</a></li>";
+			$sub_top_li2 = "<li class='sub_top_li sub_top_li2'><a onClick='tabBtn(\"2\")'>나의 로또 당첨 현황</a></li>";
+			$sub_top_li3 = "<li class='sub_top_li sub_top_li3'><a onClick='tabBtn(\"3\")'>나의 로또 보관함</a></li>";
+			$sub_tit = "나의 당첨현황";
+			$sub_top_bg = "sub_top_bg3";
 		break;
 
 		case "my_info.php":
-			$sub_top = "my_bg.jpg";
-			$sub_title = "나의로또";
+			$s01_li2 = "마이페이지";
+			$sub_top_li1 = "<li class='active'><a>회원정보</a></li>";
 			$sub_tit = "회원정보 수정";
-		break;
-		case "my_lotto.php":
-			$sub_top = "my_bg.jpg";
-			$sub_title = "나의로또";
-			$sub_tit = "나의 당첨현황";
-		break;
-		case "my_lotto02.php":
-			$sub_top = "my_bg.jpg";
-			$sub_title = "나의로또";
-			$sub_tit = "나의 로또 당첨 현황";
-		break;
-		case "my_lotto03.php":
-			$sub_top = "my_bg.jpg";
-			$sub_title = "나의로또";
-			$sub_tit = "나의 로또 보관함";
+			$sub_top_bg = "sub_top_bg3";
 		break;
 
-		case "notmessage.php":
-			$inner = "inner100";
-			$pad0 = "padding0";
+		case "prize.php":
+			$inner_x = "inner_x";
 		break;
-		case "perfect_member.php":
-			$addClass = "perfect_inner";
-			$inner = "overf";
-			$sub_top = "perfect_member_bg.jpg";
-			$sub_title = "모두가 기다리고 기다렸던 로또1등 당첨의 끝판왕! PERFECT!<br>기존 TOP-CLASS등급의 Change-Up!";
-			//$sub_tit = "나의 로또 보관함";
+
+		case "deluxe.php":
+			$inner_x = "inner_x";
 		break;
-		case "perfect_member_.php":
-			$addClass = "perfect_inner";
-			$inner = "overf";
-			$sub_top = "perfect_member_bg.jpg";
-			$sub_title = "모두가 기다리고 기다렸던 로또1등 당첨의 끝판왕! PERFECT!<br>기존 TOP-CLASS등급의 Change-Up!";
-			//$sub_tit = "나의 로또 보관함";
-		break;
+
 	}
 
 	switch($bo_table){
-		case "notice":
-			$sub_top = "cs_bg.jpg";
-			$sub_title = "고객센터";
-			$sub_tit = "공지사항";
-		break;
+
+	case "notice":
+		$s01_li2 = "고객센터";
+		$sub_top_li1 = "<li class='active'><a href='/bbs/board.php?bo_table=notice'>공지사항</a></li>";
+		$sub_top_li2 = "<li><a href='/bbs/board.php?bo_table=faq'>자주묻는 질문</a></li>";
+		$sub_top_li3 = "<li><a href='/bbs/qalist.php'>1:1 상담</a></li>";
+		$sub_tit = "공지사항";
+		$sub_top_bg = "sub_top_bg1";
+	break;
+
+	case "faq":
+		$s01_li2 = "고객센터";
+		$sub_top_li1 = "<li><a href='/bbs/board.php?bo_table=notice'>공지사항</a></li>";
+		$sub_top_li2 = "<li class='active'><a href='/bbs/board.php?bo_table=faq'>자주묻는 질문</a></li>";
+		$sub_top_li3 = "<li><a href='/bbs/qalist.php'>1:1 상담</a></li>";
+		$sub_tit = "자주묻는 질문";
+		$sub_top_bg = "sub_top_bg1";
+	break;
+
 	}
+
+	switch($co_id){
+
+	case "privacy":
+		$s01_li2 = "개인정보처리방침";
+		$sub_top_li1 = "<li class='active'><a href='/bbs/content.php?co_id=privacy'>개인정보처리방침</a></li>";
+		$sub_top_li2 = "<li><a href='/bbs/content.php?co_id=provision'>이용약관</a></li>";
+		$sub_tit = "개인정보처리방침";
+		$sub_top_bg = "sub_top_bg1";
+	break;
+
+	case "provision":
+		$s01_li2 = "이용약관";
+		$sub_top_li1 = "<li><a href='/bbs/content.php?co_id=privacy'>개인정보처리방침</a></li>";
+		$sub_top_li2 = "<li class='active'><a href='/bbs/content.php?co_id=provision'>이용약관</a></li>";
+		$sub_tit = "이용약관";
+		$sub_top_bg = "sub_top_bg1";
+	break;
+
+	}
+
 ?>
