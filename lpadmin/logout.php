f7f3af76b240c874eaa6a13b64e6a6abc3c9d906
rowoonpts-lang <rowoon.pts@gmail.com>
2026-08-21 03:29:28 +0000

    fix: allow admin logout outside IP restrictions

--- lpadmin/logout.php
+++ lpadmin/logout.php
@@ -1,13 +1,21 @@
 <?php
-	include_once("_common.php");
 
-	session_unset(); // 모든 세션변수를 언레지스터 시켜줌
-	session_destroy(); // 세션해제함
+/*
+ * 로그아웃은 관리자 IP 제한과 관계없이 항상 실행할 수 있어야 하므로
+ * lpadmin/_common.php 대신 사이트 공통 파일을 직접 불러옵니다.
+ */
+include_once("../common.php");
 
-	// 자동로그인 해제 --------------------------------
-	set_cookie('ck_mb_id', '', 0);
-	set_cookie('ck_auto', '', 0);
-	// 자동로그인 해제 end --------------------------------
+session_unset();
+session_destroy();
 
-	goto_url(G5_LADMIN_URL."/login.php");
+/* 자동로그인 해제 */
+set_cookie('ck_mb_id', '', 0);
+set_cookie('ck_auto', '', 0);
+
+/*
+ * 관리자 전용 로그인 화면이 아니라
+ * 홈페이지 첫 화면으로 이동합니다.
+ */
+goto_url(G5_URL);
 ?>
